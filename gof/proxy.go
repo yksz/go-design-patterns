84521537341f5004c27b34b73c5a0abe9129450c
yksz <user4git@gmail.com>
2014-02-01 04:24:17 +0900

    Add proxy pattern

---
+++ gof/proxy.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Subject interface {
+	DoAction()
+}
+
+type RealSubject struct {
+}
+
+func (r *RealSubject) DoAction() {
+	fmt.Println("+- RealSubject.DoAction()")
+}
+
+type Proxy struct {
+	realSubject *RealSubject
+}
+
+func (p *Proxy) DoAction() {
+	if p.realSubject == nil {
+		p.realSubject = new(RealSubject)
+	}
+	fmt.Println("Proxy.DoAction()")
+	p.realSubject.DoAction()
+}
+
+func main() {
+	subject := new(Proxy)
+	subject.DoAction()
+}
