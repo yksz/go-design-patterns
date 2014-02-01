2c2248f570f2667716dc57f2b7c371d04bab3c8c
yksz <user4git@gmail.com>
2014-02-02 00:16:40 +0900

    Add factory method pattern

---
+++ gof/factory_method.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Creator struct {
+	factory factory
+}
+
+func (c *Creator) Operation() {
+	product := c.factory.factoryMethod()
+	product.method()
+}
+
+type factory interface {
+	factoryMethod() Product
+}
+
+type ConcreteCreator struct {
+}
+
+func (c *ConcreteCreator) factoryMethod() Product {
+	return new(ConcreteProduct)
+}
+
+type Product interface {
+	method()
+}
+
+type ConcreteProduct struct {
+}
+
+func (c *ConcreteProduct) method() {
+	fmt.Println("ConcreteProduct.method()")
+}
+
+func main() {
+	creator := Creator{new(ConcreteCreator)}
+	creator.Operation()
+}
