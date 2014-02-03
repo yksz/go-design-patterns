c06c409c03ec5ab7e5650659005c8f4a211b4506
yksz <user4git@gmail.com>
2014-02-03 11:54:05 +0900

    Add composite pattern

---
+++ gof/composite.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Component interface {
+	Operation(int)
+}
+
+type File struct {
+	name string
+}
+
+func (f *File) Operation(depth int) {
+	for i := 0; i < depth; i++ {
+		fmt.Print(" ")
+	}
+	fmt.Println("File: " + f.name)
+}
+
+type Directory struct {
+	components []Component
+	name       string
+}
+
+func (d *Directory) Operation(depth int) {
+	for i := 0; i < depth; i++ {
+		fmt.Print(" ")
+	}
+	fmt.Println("Directory: " + d.name)
+	for _, component := range d.components {
+		component.Operation(depth + 1)
+	}
+}
+
+func (d *Directory) Add(component Component) {
+	d.components = append(d.components, component)
+}
+
+func (d *Directory) Remove(component Component) {
+	for i, v := range d.components {
+		if v == component {
+			d.delete(i)
+			return
+		}
+	}
+}
+
+func (d *Directory) delete(i int) {
+	reslice := append(d.components[:i], d.components[i+1:]...)
+	newslice := make([]Component, len(reslice))
+	copy(newslice, reslice)
+	d.components = newslice
+}
+
+func (d *Directory) GetChildren() []Component {
+	return d.components
+}
+
+func main() {
+	root := &Directory{name: "root"}
+	usr := &Directory{name: "usr"}
+	local := &Directory{name: "local"}
+	home := &Directory{name: "home"}
+	user1 := &Directory{name: "user1"}
+	file1 := &File{name: "file1"}
+
+	root.Add(usr)
+	usr.Add(local)
+	root.Add(home)
+	home.Add(user1)
+	user1.Add(file1)
+	root.Operation(0)
+	root.Remove(home)
+	root.Operation(0)
+}
