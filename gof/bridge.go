cc3fc7263ce3c9ef28f6a774554635815152b802
yksz <user4git@gmail.com>
2014-02-06 10:19:53 +0900

    Add bridge pattern

---
+++ gof/bridge.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+type DrawingAPI interface {
+	DrawCircle(x, y, radius float32)
+}
+
+type DrawingAPI1 struct {
+}
+
+func (d *DrawingAPI1) DrawCircle(x, y, radius float32) {
+	fmt.Printf("API1.circle at %f:%f radius %f\n", x, y, radius)
+}
+
+type DrawingAPI2 struct {
+}
+
+func (d *DrawingAPI2) DrawCircle(x, y, radius float32) {
+	fmt.Printf("API2.circle at %f:%f radius %f\n", x, y, radius)
+}
+
+type Shape interface {
+	Draw()
+	ResizeByPercentage(pct float32)
+}
+
+type CircleShape struct {
+	x, y, radius float32
+	drawingAPI   DrawingAPI
+}
+
+func (c *CircleShape) Draw() {
+	c.drawingAPI.DrawCircle(c.x, c.y, c.radius)
+}
+
+func (c *CircleShape) ResizeByPercentage(pct float32) {
+	c.radius *= pct
+}
+
+func main() {
+	shapes := []Shape{&CircleShape{1, 2, 3, new(DrawingAPI1)}, &CircleShape{5, 7, 11, new(DrawingAPI2)}}
+	for _, shape := range shapes {
+		shape.ResizeByPercentage(2.5)
+		shape.Draw()
+	}
+}
