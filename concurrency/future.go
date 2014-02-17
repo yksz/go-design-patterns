f4e51f4f4e02ffd66592695e3155a2ff18f3d7f3
yksz <user4git@gmail.com>
2014-02-17 14:58:13 +0900

    Add future pattern

---
+++ concurrency/future.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Future chan string
+
+type Executor struct{}
+
+func (e *Executor) Submit(fn func() string) Future {
+	future := make(Future)
+	go func() {
+		val := fn()
+		future <- val
+	}()
+	return future
+}
+
+func main() {
+	task := func() string {
+		fmt.Println("Task: Now processing...")
+		time.Sleep(2 * time.Second)
+		fmt.Println("Task: Process is completed.")
+		return "Task has finished!"
+	}
+	executor := new(Executor)
+	future := executor.Submit(task)
+	fmt.Printf("Future result: %s\n", <-future)
+}
