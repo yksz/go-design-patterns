21cdddf275f43ee39a990a2fbeae4ae6835fbb50
yksz <user4git@gmail.com>
2014-02-18 12:18:01 +0900

    Add object pool pattern

---
+++ creation/object_pool.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"container/list"
+	"fmt"
+	"strconv"
+)
+
+var (
+	uniqueID int
+)
+
+type PooledObject struct {
+	id int
+}
+
+func (o *PooledObject) String() string {
+	return "PooledObject [id=" + strconv.Itoa(o.id) + "]"
+}
+
+type ObjectPool struct {
+	idle   *list.List
+	active *list.List
+}
+
+func NewObjectPool() *ObjectPool {
+	idle := list.New()
+	active := list.New()
+	return &ObjectPool{idle, active}
+}
+
+func (p *ObjectPool) BorrowObject() *PooledObject {
+	var object *PooledObject
+	if p.idle.Len() <= 0 {
+		object = &PooledObject{uniqueID}
+		uniqueID++
+	} else {
+		object = p.removeAt(p.idle, 0)
+	}
+	fmt.Printf("Borrow: %s\n", object)
+	p.active.PushBack(object)
+	return object
+}
+
+func (p *ObjectPool) ReturnObject(object *PooledObject) {
+	fmt.Printf("Return: %s\n", object)
+	p.idle.PushBack(object)
+	p.remove(p.active, object)
+}
+
+func (p *ObjectPool) remove(list *list.List, object *PooledObject) {
+	for e := list.Front(); e != nil; e = e.Next() {
+		if object == e.Value.(*PooledObject) {
+			list.Remove(e)
+			return
+		}
+	}
+}
+
+func (p *ObjectPool) removeAt(list *list.List, index int) *PooledObject {
+	for e, i := list.Front(), 0; e != nil; e, i = e.Next(), i+1 {
+		if index == i {
+			return list.Remove(e).(*PooledObject)
+		}
+	}
+	return nil
+}
+
+func main() {
+	objectPool := NewObjectPool()
+	object1 := objectPool.BorrowObject()
+	objectPool.ReturnObject(object1)
+	object2 := objectPool.BorrowObject()
+	object3 := objectPool.BorrowObject()
+	objectPool.ReturnObject(object2)
+	objectPool.ReturnObject(object3)
+}
